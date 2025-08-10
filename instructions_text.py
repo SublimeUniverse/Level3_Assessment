9545ea74dc115f6f524b3140ff4fc02457f9573e
SublimeUniverse <100240793+SublimeUniverse@users.noreply.github.com>
2025-08-11 11:56:27 +1200

    Create instructions_text.py

---
+++ instructions_text.py
@@ -0,0 +1,7 @@
+instructions = """Welcome to the Maths Game!
+
+You may decide the number of rounds or infinite mode
+along with the mode -  there are no negative answers!
+
+Hope you enjoy!
+                    """
